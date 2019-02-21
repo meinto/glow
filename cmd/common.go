031cc7eb496998036988e11526abb5a270b0ff8a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-02-21 17:25:50 +0100

    helper function for creating merge requests

--- cmd/common.go
+++ cmd/common.go
@@ -1,11 +1,62 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/spf13/viper"
 )
 
 func CheckRequiredStringField(val, fieldName string) {
 	if val == "" {
 		log.Fatalf("please provide %s", fieldName)
 	}
+}
+
+func CreateMergeRequest(source, target string) {
+	CheckRequiredStringField(source, "source branch")
+	CheckRequiredStringField(target, "target branch")
+	CheckRequiredStringField(viper.GetString("gitlabEndpoint"), "gitlab endpoint")
+	CheckRequiredStringField(viper.GetString("projectNamespace"), "project namespace")
+	CheckRequiredStringField(viper.GetString("projectName"), "project name")
+	CheckRequiredStringField(viper.GetString("gitlabCIToken"), "gitlab ci token")
+
+	type Payload struct {
+		SourceBranch       string `json:"source_branch"`
+		TargetBranch       string `json:"target_branch"`
+		Title              string `json:"title"`
+		RemoveSourceBranch bool   `json:"remove_source_branch"`
+	}
+
+	data := Payload{
+		SourceBranch:       source,
+		TargetBranch:       target,
+		Title:              fmt.Sprintf("Merge %s in %s", source, target),
+		RemoveSourceBranch: false,
+	}
+	payloadBytes, err := json.Marshal(data)
+	CheckForError(err, "mergeRequestCmd marshal json")
+
+	body := bytes.NewReader(payloadBytes)
+
+	requestURI := fmt.Sprintf(
+		"%s/api/v4/projects/%s%s%s/merge_requests",
+		viper.GetString("gitlabEndpoint"),
+		viper.GetString("projectNamespace"),
+		"%2F",
+		viper.GetString("projectName"),
+	)
+	req, err := http.NewRequest("POST", requestURI, body)
+	CheckForError(err, "mergeRequestCmd prepare request")
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Private-Token", viper.GetString("gitlabCIToken"))
+
+	resp, err := http.DefaultClient.Do(req)
+	CheckForError(err, "mergeRequestCmd do request")
+
+	defer resp.Body.Close()
 }
--- cmd/mergeRequest.go
+++ cmd/mergeRequest.go
@@ -1,11 +1,7 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -48,41 +44,6 @@ var mergeRequestCmd = &cobra.Command{
 		CheckRequiredStringField(mergeRequestCmdOptions.ProjectName, "project name")
 		CheckRequiredStringField(mergeRequestCmdOptions.GitlabCIToken, "gitlab ci token")
 
-		type Payload struct {
-			SourceBranch       string `json:"source_branch"`
-			TargetBranch       string `json:"target_branch"`
-			Title              string `json:"title"`
-			RemoveSourceBranch bool   `json:"remove_source_branch"`
-		}
-
-		data := Payload{
-			SourceBranch:       source,
-			TargetBranch:       target,
-			Title:              fmt.Sprintf("Merge %s in %s", source, target),
-			RemoveSourceBranch: false,
-		}
-		payloadBytes, err := json.Marshal(data)
-		CheckForError(err, "mergeRequestCmd marshal json")
-
-		body := bytes.NewReader(payloadBytes)
-
-		requestURI := fmt.Sprintf(
-			"%s/api/v4/projects/%s%s%s/merge_requests",
-			viper.GetString("gitlabEndpoint"),
-			viper.GetString("projectNamespace"),
-			"%2F",
-			viper.GetString("projectName"),
-		)
-		req, err := http.NewRequest("POST", requestURI, body)
-		CheckForError(err, "mergeRequestCmd prepare request")
-
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Private-Token", viper.GetString("gitlabCIToken"))
-
-		resp, err := http.DefaultClient.Do(req)
-		CheckForError(err, "mergeRequestCmd do request")
-
-		defer resp.Body.Close()
-
+		CreateMergeRequest(source, target)
 	},
 }
