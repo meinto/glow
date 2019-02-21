4b222d6fa892a2a0ff8062e149be0c2673421873
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-02-21 22:10:01 +0100

    build for different os and architectures

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 mkdir build
-GOOS=darwin go build -o build/glow .
+GOOS=darwin GOARCH=386 go build -o build/glow_darwin_386/glow .
+GOOS=darwin GOARCH=amd64 go build -o build/glow_darwin_amd64/glow .
+GOOS=linux GOARCH=386 go build -o build/glow_linux_386/glow .
+GOOS=linux GOARCH=amd64 go build -o build/glow_linux_amd64/glow .
+GOOS=windows GOARCH=386 go build -o build/glow_windows_386/glow.exe .
+GOOS=windows GOARCH=amd64 go build -o build/glow_windows_amd64/glow.exe .
