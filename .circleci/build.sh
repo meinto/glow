cd0c02ac01e91124d29cd1412866b702d3854727
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-02-21 22:15:44 +0100

    rename build files

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir build
-GOOS=darwin GOARCH=386 go build -o build/glow_darwin_386/glow .
-GOOS=darwin GOARCH=amd64 go build -o build/glow_darwin_amd64/glow .
-GOOS=linux GOARCH=386 go build -o build/glow_linux_386/glow .
-GOOS=linux GOARCH=amd64 go build -o build/glow_linux_amd64/glow .
-GOOS=windows GOARCH=386 go build -o build/glow_windows_386/glow.exe .
-GOOS=windows GOARCH=amd64 go build -o build/glow_windows_amd64/glow.exe .
+GOOS=darwin GOARCH=386 go build -o build/glow_darwin_386 .
+GOOS=darwin GOARCH=amd64 go build -o build/glow_darwin_amd64 .
+GOOS=linux GOARCH=386 go build -o build/glow_linux_386 .
+GOOS=linux GOARCH=amd64 go build -o build/glow_linux_amd64 .
+GOOS=windows GOARCH=386 go build -o build/glow_windows_386.exe .
+GOOS=windows GOARCH=amd64 go build -o build/glow_windows_amd64.exe .
