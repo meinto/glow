bd40debb2019e4f65caf093a9ec51a86bd2b5c8a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-08 23:35:50 +0100

    implement fix cmd

--- pkg/cli/cmd/feature.go
+++ pkg/cli/cmd/feature.go
@@ -30,29 +30,5 @@ var featureCmd = &cobra.Command{
 
 		g.Checkout(feature)
 		util.CheckForError(err, "Checkout")
-
-		// r, err := git.PlainOpen(".")
-		// util.CheckForError(err, "PlainOpen")
-
-		// headRef, err := r.Head()
-		// util.CheckForError(err, "Head")
-
-		// refName := string(headRef.Name())
-		// if !strings.Contains(refName, "develop") {
-		// 	log.Println("You are not on the develop branch.")
-		// 	log.Fatalf("Please switch branch...")
-		// }
-
-		// branchName := fmt.Sprintf("refs/heads/feature/%s/%s", viper.GetString("author"), feature)
-		// ref := plumbing.NewHashReference(plumbing.ReferenceName(branchName), headRef.Hash())
-
-		// err = r.Storer.SetReference(ref)
-		// util.CheckForError(err, "SetReference")
-
-		// w, err := r.Worktree()
-		// util.CheckForError(err, "Worktree")
-
-		// err = util.Checkout(w, branchName, util.ShouldUseNativeGitBinding("checkout"))
-		// util.CheckForError(err, "Checkout")
 	},
 }
