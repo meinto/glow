503ab06cf2836efc89d4d42d9904d395fd163077
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-08 03:28:39 +0100

    implement value receivers

--- fix.go
+++ fix.go
@@ -1,18 +1,61 @@
 package glow
 
-import "fmt"
+import (
+	"strings"
 
-type fix struct {
-	author     string
-	name       string
-	branchName string
+	"github.com/pkg/errors"
+)
+
+type Fix struct {
+	AuthoredBranch
 }
 
-func NewFix(author, name string) fix {
-	branchName := fmt.Sprintf("refs/heads/fix/%s/%s", author, name)
-	return fix{
-		author,
-		name,
-		branchName,
+// NewFix creates a new fix definition
+func NewFix(author, name string) (Fix, error) {
+	ab, err := NewAuthoredBranch("refs/heads/fix/%s/%s", author, name)
+	if err != nil {
+		return Fix{}, errors.Wrap(err, "error while creating fix definition")
 	}
+	return Fix{ab}, nil
+}
+
+// FixFromBranch extracts a fix definition from branch name
+func FixFromBranch(branchName string) (Fix, error) {
+	if !strings.Contains(branchName, "/fix/") {
+		return Fix{}, errors.New("no valid fix branch")
+	}
+	ab, err := AuthoredBranchFromBranchName(branchName)
+	if err != nil {
+		return Fix{}, errors.Wrap(err, "error while creating fix definition from branch name")
+	}
+	return Fix{ab}, nil
+}
+
+// CreationIsAllowedFrom returns wheter branch is allowed to be created
+// from given this source branch
+func (f Fix) CreationIsAllowedFrom(sourceBranch string) bool {
+	if strings.Contains(sourceBranch, "release/v") {
+		return true
+	}
+	return false
+}
+
+// CanBeClosed checks if the branch name is a valid
+func (f Fix) CanBeClosed() bool {
+	return true
+}
+
+// CanBePublished checks if the branch can be published directly to production
+func (f Fix) CanBePublished() bool {
+	return false
+}
+
+// CloseBranches returns all branches which this branch have to be merged with
+func (f Fix) CloseBranches(availableBranches []string) []string {
+	return []string{}
+}
+
+// PublishBranch returns the publish branch if available
+func (f Fix) PublishBranch() string {
+	return ""
 }
