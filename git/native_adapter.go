ec4f5d6f1e5401b59188c4b2728f605b172da91f
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-08 22:47:09 +0100

    implement checkout method of gogit adapter

--- git/native_adapter.go
+++ git/native_adapter.go
@@ -5,16 +5,20 @@ import (
 	"github.com/pkg/errors"
 )
 
+// NativeGitAdapter implemented with native git
 type NativeGitAdapter struct{}
 
+// CurrentBranch returns the current branch name
 func (g NativeGitAdapter) CurrentBranch() (glow.Branch, error) {
 	return glow.Branch{}, errors.New("not implemented yet")
 }
 
+// Create a new branch
 func (g NativeGitAdapter) Create(b glow.IBranch) error {
 	return errors.New("not implemented yet")
 }
 
+// Checkout a branch
 func (g NativeGitAdapter) Checkout(b glow.IBranch) error {
 	return errors.New("not implemented yet")
 }
