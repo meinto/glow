677140bc3d734067d5c4402d64a1e303a8df73e9
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-09 01:40:47 +0100

    put git service in git package & rename service creators

---
+++ git/service.go
@@ -0,0 +1,22 @@
+package git
+
+import "github.com/meinto/glow"
+
+// Service describes all actions which can performed with git
+type Service interface {
+	CurrentBranch() (glow.Branch, error)
+	Create(b glow.IBranch) error
+	Checkout(b glow.IBranch) error
+}
+
+type service struct {
+	Service
+}
+
+func NewGoGitService() service {
+	return service{GoGitAdapter{}}
+}
+
+func NewNativeService(gitPath string) service {
+	return service{NativeGitAdapter{gitPath}}
+}
