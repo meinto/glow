77041c1242d19769f3c1b83eef79c13f0e983ff9
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-09 16:36:45 +0100

    implement githost logging service methods

--- githost/logging.go
+++ githost/logging.go
@@ -1,9 +1,10 @@
 package githost
 
 import (
+	"time"
+
 	"github.com/go-kit/kit/log"
 	"github.com/meinto/glow"
-	"github.com/pkg/errors"
 )
 
 type loggingService struct {
@@ -15,10 +16,16 @@ func NewLoggingService(l log.Logger, s Service) Service {
 	return &loggingService{l, s}
 }
 
-func (s *loggingService) Close(b glow.Branch) error {
-	return errors.New("not implemented yet")
+func (s *loggingService) Close(b glow.Branch) (err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("method", "Close", "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.next.Close(b)
 }
 
-func (s *loggingService) Publish(b glow.Branch) error {
-	return errors.New("not implemented yet")
+func (s *loggingService) Publish(b glow.Branch) (err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("method", "Publish", "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.next.Publish(b)
 }
