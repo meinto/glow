a01b69ba86909d7a9c7114ee8cbdd463c18b039a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-09 15:08:10 +0100

    implement merge request

--- githost/gitlab_adapter.go
+++ githost/gitlab_adapter.go
@@ -1,6 +1,12 @@
 package githost
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/meinto/glow"
 	"github.com/pkg/errors"
 )
@@ -11,53 +17,71 @@ type gitlabAdapter struct {
 
 func (a *gitlabAdapter) Close(b glow.Branch) error {
 	if b.CanBeClosed() {
-		// a.createMergeRequest(b)
+		return a.createMergeRequest(b)
 	}
-	return errors.New("not implemented yet")
+	return errors.New("cannot be closed")
 }
 
 func (a *gitlabAdapter) Publish(b glow.Branch) error {
-	return errors.New("not implemented yet")
+	if b.CanBePublished() {
+		return a.createMergeRequest(b)
+	}
+	return errors.New("cannot be published")
 }
 
-// func (a *gitlabAdapter) createMergeRequest(b glow.Branch) error {
-// 	type Payload struct {
-// 		SourceBranch       string `json:"source_branch"`
-// 		TargetBranch       string `json:"target_branch"`
-// 		Title              string `json:"title"`
-// 		RemoveSourceBranch bool   `json:"remove_source_branch"`
-// 	}
+func (a *gitlabAdapter) createMergeRequest(b glow.Branch) error {
+	type Payload struct {
+		SourceBranch       string `json:"source_branch"`
+		TargetBranch       string `json:"target_branch"`
+		Title              string `json:"title"`
+		RemoveSourceBranch bool   `json:"remove_source_branch"`
+	}
 
-// 	currentBranch := a.gitService.CurrentBranch()
-// 	targets := b.CloseBranches()
+	branchList, err := a.gitService.BranchList()
+	if err != nil {
+		return errors.Wrap(err, "error getting branch list")
+	}
+	targets := b.CloseBranches(branchList)
 
-// 	data := Payload{
-// 		SourceBranch:       b.ShortBranchName(),
-// 		TargetBranch:       target.ShortBranchName(),
-// 		Title:              fmt.Sprintf("Merge %s in %s", source.ShortBranchName(), target.ShortBranchName()),
-// 		RemoveSourceBranch: false,
-// 	}
-// 	payloadBytes, err := json.Marshal(data)
-// 	CheckForError(err, "mergeRequestCmd marshal json")
+	for _, t := range targets {
+		source := b
+		target := t
 
-// 	body := bytes.NewReader(payloadBytes)
+		data := Payload{
+			SourceBranch:       source.ShortBranchName(),
+			TargetBranch:       target.ShortBranchName(),
+			Title:              fmt.Sprintf("Merge %s in %s", source.ShortBranchName(), target.ShortBranchName()),
+			RemoveSourceBranch: false,
+		}
+		payloadBytes, err := json.Marshal(data)
+		if err != nil {
+			return errors.Wrap(err, "marshal json")
+		}
 
-// 	requestURI := fmt.Sprintf(
-// 		"%s/api/v4/projects/%s%s%s/merge_requests",
-// 		a.endpoint,
-// 		a.namespace,
-// 		"%2F",
-// 		a.project,
-// 	)
-// 	req, err := http.NewRequest("POST", requestURI, body)
-// 	CheckForError(err, "mergeRequestCmd prepare request")
+		body := bytes.NewReader(payloadBytes)
 
-// 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Private-Token", a.token)
+		requestURI := fmt.Sprintf(
+			"%s/api/v4/projects/%s%s%s/merge_requests",
+			a.endpoint,
+			a.namespace,
+			"%2F",
+			a.project,
+		)
+		req, err := http.NewRequest("POST", requestURI, body)
+		if err != nil {
+			return errors.Wrap(err, "prepare request")
+		}
 
-// 	resp, err := http.DefaultClient.Do(req)
-// 	CheckForError(err, "mergeRequestCmd do request")
-// 	defer resp.Body.Close()
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Private-Token", a.token)
 
-// 	log.Printf("created merge request of %s into %s", source.ShortBranchName(), target.ShortBranchName())
-// }
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return errors.Wrap(err, "do request")
+		}
+		defer resp.Body.Close()
+
+		log.Printf("created merge request of %s into %s", source.ShortBranchName(), target.ShortBranchName())
+	}
+	return nil
+}
