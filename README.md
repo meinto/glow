e0a0c906825a60aa19de0f18031664756b17e96c
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-11 19:27:55 +0100

    rename githost to gitprovider

--- README.md
+++ README.md
@@ -77,7 +77,7 @@ glow init
 ```json
 {
   "author": "dvadar",
-  "githost": "https://gitlab.com",
+  "gitProviderDomain": "https://gitlab.com",
   "projectNamespace": "my-namespace",
   "projectName": "my-project",
   "gitlabCIToken": "abc",
--- pkg/cli/cmd/init.go
+++ pkg/cli/cmd/init.go
@@ -13,11 +13,11 @@ import (
 )
 
 type ConfigType struct {
-	Author           string `json:"author,omitempty"`
-	Githost          string `json:"githost,omitempty"`
-	ProjectNamespace string `json:"projectNamespace,omitempty"`
-	ProjectName      string `json:"projectName,omitempty"`
-	GitlabCIToken    string `json:"gitlabCIToken,omitempty"`
+	Author            string `json:"author,omitempty"`
+	GitProviderDomain string `json:"gitProviderDomain,omitempty"`
+	ProjectNamespace  string `json:"projectNamespace,omitempty"`
+	ProjectName       string `json:"projectName,omitempty"`
+	GitlabCIToken     string `json:"gitlabCIToken,omitempty"`
 }
 
 var configFileName = "glow.json"
@@ -36,7 +36,7 @@ var initCmd = &cobra.Command{
 			log.Fatalf("error setting author: %s", err)
 		}
 
-		githost, err := promptURL("Your git host endpoint (%s) ", "https://gitlab.com")
+		gitProviderDomain, err := promptURL("Your git host endpoint (%s) ", "https://gitlab.com")
 		if err != nil {
 			log.Fatalf("error setting gitlab endpoint: %s", err)
 		}
@@ -69,15 +69,15 @@ var initCmd = &cobra.Command{
 		addToGitIgnore(configFileName)
 
 		var config = ConfigType{
-			Author:           author,
-			Githost:          githost,
-			ProjectNamespace: projectNamespace,
-			ProjectName:      projectName,
-			GitlabCIToken:    gitlabCIToken,
+			Author:            author,
+			GitProviderDomain: gitProviderDomain,
+			ProjectNamespace:  projectNamespace,
+			ProjectName:       projectName,
+			GitlabCIToken:     gitlabCIToken,
 		}
 		writeJSONFile(config, configFileName)
 
-		log.Println(config, author, githost, projectNamespace, projectName, gitlabCIToken)
+		log.Println(config, author, gitProviderDomain, projectNamespace, projectName, gitlabCIToken)
 	},
 }
 
