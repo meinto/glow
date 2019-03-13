ca591615cb35cc8b8a23f6070ecefcf0a76adceb
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-13 21:15:33 +0100

    new commands for cleanup branches & tags

---
+++ pkg/cli/cmd/cleanup.tags.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/meinto/glow/pkg/cli/cmd/util"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cleanupCmd.AddCommand(cleanupTagsCmd)
+}
+
+var cleanupTagsCmd = &cobra.Command{
+	Use:   "tags",
+	Short: "cleanup tags",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		g, err := util.GetGitClient()
+		util.CheckForError(err, "GetGitClient")
+
+		err = g.CleanupTags(cleanupCmdFlags.cleanupUntracked)
+		util.CheckForError(err, "Close")
+	},
+}
