03997fb3a99d34900e00d8858a8c0cd3da70830a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-13 23:12:59 +0100

    remove cleanup root implementation

--- pkg/cli/cmd/cleanup.go
+++ pkg/cli/cmd/cleanup.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"github.com/meinto/glow/pkg/cli/cmd/util"
 	"github.com/spf13/cobra"
 )
 
@@ -19,12 +18,5 @@ func init() {
 var cleanupCmd = &cobra.Command{
 	Use:   "cleanup",
 	Short: "cleanup branches",
-	Run: func(cmd *cobra.Command, args []string) {
-
-		g, err := util.GetGitClient()
-		util.CheckForError(err, "GetGitClient")
-
-		err = g.CleanupBranches(cleanupCmdFlags.cleanupGone, cleanupCmdFlags.cleanupUntracked)
-		util.CheckForError(err, "Close")
-	},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
