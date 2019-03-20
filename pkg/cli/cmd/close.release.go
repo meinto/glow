30ccdd996c780c3ed0f246ace0d5283f553dce69
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-20 18:14:19 +0100

    Possibility to close release with current set version number

--- pkg/cli/cmd/close.release.go
+++ pkg/cli/cmd/close.release.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"github.com/meinto/glow"
 	"github.com/meinto/glow/pkg/cli/cmd/util"
+	"github.com/meinto/glow/semver"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
@@ -17,6 +19,24 @@ var closeReleaseCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		version := args[0]
+
+		if version == "current" {
+			g, err := util.GetGitClient()
+			util.CheckForError(err, "GetGitClient")
+
+			pathToRepo, err := g.GitRepoPath()
+			util.CheckForError(err, "semver GitRepoPath")
+
+			s := semver.NewSemverService(
+				pathToRepo,
+				viper.GetString("gitPath"),
+				releaseCmdOptions.VersionFile,
+				releaseCmdOptions.VersionFileType,
+			)
+			v, err := s.GetCurrentVersion(args[0])
+			util.CheckForError(err, "semver GetCurrentVersion")
+			version = v
+		}
 
 		gp, err := util.GetGitProvider()
 		util.CheckForError(err, "GetGitProvider")
