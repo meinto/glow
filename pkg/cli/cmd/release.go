6fda5c3bf387b84d297ab225c2475d111cc92d6b
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-20 11:52:46 +0100

    enhance error logging in post release script execution

--- pkg/cli/cmd/release.go
+++ pkg/cli/cmd/release.go
@@ -19,16 +19,16 @@ var releaseCmdOptions struct {
 	Push               bool
 	PostReleaseScript  string
 	PostReleaseCommand []string
-	VersionFile        string  
-	VersionFileType    string 
+	VersionFile        string
+	VersionFileType    string
 }
 
 func init() {
-	rootCmd.AddCommand(releaseCmd) 
+	rootCmd.AddCommand(releaseCmd)
 	releaseCmd.Flags().BoolVar(&releaseCmdOptions.Push, "push", false, "push created release branch")
 	releaseCmd.Flags().StringVar(&releaseCmdOptions.PostReleaseScript, "postRelease", "", "script that executes after switching to release branch")
 	releaseCmd.Flags().StringArrayVar(&releaseCmdOptions.PostReleaseCommand, "postReleaseCommand", []string{}, "commands which should be executed after switching to release branch")
-	
+
 	releaseCmd.Flags().StringVar(&releaseCmdOptions.VersionFile, "versionFile", "VERSION", "name of git-semver version file")
 	releaseCmd.Flags().StringVar(&releaseCmdOptions.VersionFileType, "versionFileType", "raw", "git-semver version file type")
 }
@@ -48,7 +48,7 @@ var releaseCmd = &cobra.Command{
 			pathToRepo, err := g.GitRepoPath()
 			util.CheckForError(err, "semver GitRepoPath")
 			s = semver.NewSemverService(
-				pathToRepo, 
+				pathToRepo,
 				viper.GetString("gitPath"),
 				releaseCmdOptions.VersionFile,
 				releaseCmdOptions.VersionFileType,
@@ -59,7 +59,7 @@ var releaseCmd = &cobra.Command{
 		}
 
 		release, err := glow.NewRelease(version)
-		util.CheckForError(err, "NewRelease") 
+		util.CheckForError(err, "NewRelease")
 
 		err = g.Create(release)
 		util.CheckForError(err, "Create")
@@ -125,12 +125,13 @@ func postRelease(version string) {
 
 func execute(version, command string) {
 	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf(command, version))
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	var stdout, stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
 	err := cmd.Run()
 	if err != nil {
-		log.Println("error while executing post-release script", err)
+		log.Println("error while executing post-release script", err.Error(), stderr.String())
 	}
 	log.Println("post release:")
-	log.Println(out.String())
+	log.Println(stdout.String())
 }
