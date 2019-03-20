948ed409a203c228608ecabc3d60f00c0346283d
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-20 18:47:01 +0100

    semver service: create tag of current version

---
+++ pkg/cli/cmd/tag.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/meinto/glow/pkg/cli/cmd/util"
+	"github.com/meinto/glow/semver"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	rootCmd.AddCommand(tagCmd)
+}
+
+var tagCmd = &cobra.Command{
+	Use:   "tag",
+	Short: "create a tag of current version",
+	Run: func(cmd *cobra.Command, args []string) {
+		g, err := util.GetGitClient()
+		util.CheckForError(err, "GetGitClient")
+
+		pathToRepo, err := g.GitRepoPath()
+		util.CheckForError(err, "semver GitRepoPath")
+
+		s := semver.NewSemverService(
+			pathToRepo,
+			viper.GetString("gitPath"),
+			releaseCmdOptions.VersionFile,
+			releaseCmdOptions.VersionFileType,
+		)
+
+		err = s.TagCurrentVersion()
+		util.CheckForError(err, "TagCurrentVersion")
+	},
+}
