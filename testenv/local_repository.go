df0bde4ffe832da66ec154867712f47cf56fc226
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-30 21:07:21 +0100

    Test for CurrentBranch

--- testenv/local_repository.go
+++ testenv/local_repository.go
@@ -1,6 +1,7 @@
 package testenv
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -19,7 +20,17 @@ func (r *LocalRepository) Init(pathToBare string) {
 	r.Do("git init")
 	r.Do("git remote add origin %s", pathToBare)
 	r.Do("echo '1.2.3' > VERSION")
+	r.Do("mkdir subfolder")
+	r.Do("touch subfolder/somefile")
 	r.Do("git add .")
 	r.Do("git commit -m 'initial commit'")
 	r.Do("git push -u origin master")
+}
+
+func (r *LocalRepository) CreateBranch(branch string) {
+	r.Do(fmt.Sprintf("git branch %s", branch))
+}
+
+func (r *LocalRepository) Checkout(branch string) {
+	r.Do(fmt.Sprintf("git checkout %s", branch))
 }
