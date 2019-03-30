449f8432101b39b0c2075281a23cd94231062677
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-30 16:27:39 +0100

    Add test for GetNextVersion

--- semver/service_test.go
+++ semver/service_test.go
@@ -19,9 +19,34 @@ func TestGetCurrentVersion(t *testing.T) {
 	defer teardown()
 
 	s := setupSemverService(local.Folder)
-	currentVersion, _ := s.GetCurrentVersion()
+	currentVersion, err := s.GetCurrentVersion()
+	testenv.CheckForErrors(t, err)
 
-	if currentVersion != "1.2.3" {
+	if currentVersion != "1.2.3" || err != nil {
 		t.Errorf("current version should be '1.2.3' but is '%s'", currentVersion)
+	}
+}
+
+func TestGetNextVersion(t *testing.T) {
+	local, _, teardown := testenv.SetupEnv(t)
+	defer teardown()
+
+	s := setupSemverService(local.Folder)
+	v, err := s.GetNextVersion("major")
+	testenv.CheckForErrors(t, err)
+	if v != "2.0.0" || err != nil {
+		t.Errorf("version should be '2.0.0' but is '%s'", v)
+	}
+
+	v, err = s.GetNextVersion("minor")
+	testenv.CheckForErrors(t, err)
+	if v != "1.3.0" || err != nil {
+		t.Errorf("version should be '1.3.0' but is '%s'", v)
+	}
+
+	v, err = s.GetNextVersion("patch")
+	testenv.CheckForErrors(t, err)
+	if v != "1.2.4" || err != nil {
+		t.Errorf("version should be '1.2.4' but is '%s'", v)
 	}
 }
