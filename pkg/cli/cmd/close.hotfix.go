fc1a7e6f52737cc358f03268af96bc88b513bfb4
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-05-02 18:12:30 +0200

    Handle hotfixes like releases

---
+++ pkg/cli/cmd/close.hotfix.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/meinto/glow"
+	"github.com/meinto/glow/pkg/cli/cmd/util"
+	"github.com/meinto/glow/semver"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	closeCmd.AddCommand(closeHotfixCmd)
+	util.AddFlagsForMergeRequests(closeHotfixCmd)
+}
+
+var closeHotfixCmd = &cobra.Command{
+	Use:   "hofix",
+	Short: "close a release branch",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		version := args[0]
+
+		if version == "current" {
+			g, err := util.GetGitClient()
+			util.CheckForError(err, "GetGitClient")
+
+			pathToRepo, err := g.GitRepoPath()
+			util.CheckForError(err, "semver GitRepoPath")
+
+			s := semver.NewSemverService(
+				pathToRepo,
+				"/bin/bash",
+				releaseCmdOptions.VersionFile,
+				releaseCmdOptions.VersionFileType,
+			)
+			v, err := s.GetCurrentVersion()
+			util.CheckForError(err, "semver GetCurrentVersion")
+			version = v
+		}
+
+		gp, err := util.GetGitProvider()
+		util.CheckForError(err, "GetGitProvider")
+
+		currentBranch, err := glow.NewHotfix(version)
+		util.CheckForError(err, "NewHotfix")
+
+		gp.Close(currentBranch)
+		util.CheckForError(err, "Close")
+	},
+}
