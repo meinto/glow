f4a374d70611917afda81bce6023da81219fb9a5
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-05-02 18:12:30 +0200

    Handle hotfixes like releases

--- pkg/cli/cmd/hotfix.go
+++ pkg/cli/cmd/hotfix.go
@@ -4,11 +4,25 @@ import (
 	"github.com/meinto/glow"
 	"github.com/meinto/glow/pkg/cli/cmd/util"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
+
+var hotfixCmdOptions struct {
+	Push              bool
+	PostHotfixScript  string
+	PostHotfixCommand []string
+	VersionFile       string
+	VersionFileType   string
+}
 
 func init() {
 	rootCmd.AddCommand(hotfixCmd)
+
+	hotfixCmd.Flags().BoolVar(&hotfixCmdOptions.Push, "push", false, "push created hotfix branch")
+	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.PostHotfixScript, "postHofix", "", "script that executes after switching to hotfix branch")
+	hotfixCmd.Flags().StringArrayVar(&hotfixCmdOptions.PostHotfixCommand, "postHotfixCommand", []string{}, "commands which should be executed after switching to hotfix branch")
+
+	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.VersionFile, "versionFile", "VERSION", "name of git-semver version file")
+	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.VersionFileType, "versionFileType", "raw", "git-semver version file type")
 }
 
 var hotfixCmd = &cobra.Command{
@@ -16,18 +30,39 @@ var hotfixCmd = &cobra.Command{
 	Short: "create a hotfix branch",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		hotixName := args[0]
-
-		hotfix, err := glow.NewHotfix(viper.GetString("author"), hotixName)
-		util.CheckForError(err, "NewHotfix")
-
 		g, err := util.GetGitClient()
 		util.CheckForError(err, "GetGitClient")
+
+		version, s := util.ProcessVersion(
+			args[0],
+			hotfixCmdOptions.VersionFile,
+			hotfixCmdOptions.VersionFileType,
+		)
+
+		hotfix, err := glow.NewHotfix(version)
+		util.CheckForError(err, "NewHotfix")
 
 		err = g.Create(hotfix)
 		util.CheckForError(err, "Create")
 
 		g.Checkout(hotfix)
 		util.CheckForError(err, "Checkout")
+
+		if util.IsSemanticVersion(args[0]) {
+			err = s.SetNextVersion(args[0])
+			util.CheckForError(err, "semver SetNextVersion")
+		}
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		version := args[0]
+
+		util.PostRunWithVersion(
+			version,
+			hotfixCmdOptions.VersionFile,
+			hotfixCmdOptions.VersionFileType,
+			hotfixCmdOptions.PostHotfixScript,
+			hotfixCmdOptions.PostHotfixCommand,
+			hotfixCmdOptions.Push,
+		)
 	},
 }
