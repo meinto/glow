87be45933306384b540325df8dc705121eb379a0
DerZersaeger <m.renno@hornbach.com>
2020-01-17 14:04:52 +0100

    Auto detect cicdOrigin

--- pkg/cli/cmd/root.go
+++ pkg/cli/cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 
+	"net/url"
+
 	kitlog "github.com/go-kit/kit/log"
 	"github.com/gobuffalo/packr/v2"
 	"github.com/meinto/glow/pkg/cli/cmd/util"
@@ -13,11 +15,12 @@ import (
 )
 
 var rootCmdOptions struct {
-	Author     string
-	GitPath    string
-	CICDOrigin string
-	CI         bool
-	SkipChecks bool
+	Author           string
+	GitPath          string
+	CICDOrigin       string
+	DetectCICDOrigin bool
+	CI               bool
+	SkipChecks       bool
 }
 
 var logger kitlog.Logger
@@ -32,8 +35,48 @@ var rootCmd = &cobra.Command{
 			util.ExitOnError(err)
 
 			util.ExitOnError(g.SetCICDOrigin(rootCmdOptions.CICDOrigin))
+		} else if rootCmdOptions.DetectCICDOrigin {
+			g, err := util.GetGitClient()
+			util.ExitOnError(err)
+
+			cicdOrigin := detectCICDOrigin()
+			util.ExitOnError(g.SetCICDOrigin(cicdOrigin))
 		}
 	},
+}
+
+func detectCICDOrigin() string {
+	gitProviderDomain := viper.GetString("gitProviderDomain")
+	util.CheckRequiredStringField(gitProviderDomain, "gitProviderDomain")
+
+	projectNamespace := viper.GetString("projectNamespace")
+	util.CheckRequiredStringField(projectNamespace, "projectNamespace")
+
+	projectName := viper.GetString("projectName")
+	util.CheckRequiredStringField(projectName, "projectName")
+
+	gitUser := os.Getenv("CI_GIT_USER")
+	gitToken := os.Getenv("CI_GIT_TOKEN")
+
+	if gitUser != "" && gitToken != "" {
+		gitProviderURL, err := url.Parse(gitProviderDomain)
+		util.ExitOnErrorWithMessage("couldn't parse gitProviderDomain")(err)
+
+		originURL := fmt.Sprintf(
+			"%s/%s/%s.git",
+			gitProviderURL.Host, projectNamespace, projectName,
+		)
+
+		return fmt.Sprintf(
+			"%s://%s:%s@%s",
+			gitProviderURL.Scheme, gitUser, gitToken, originURL,
+		)
+	}
+
+	return fmt.Sprintf(
+		"%s/%s/%s.git",
+		gitProviderDomain, projectNamespace, projectName,
+	)
 }
 
 func init() {
@@ -48,6 +91,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&rootCmdOptions.Author, "author", "a", "", "name of the author")
 	rootCmd.PersistentFlags().StringVar(&rootCmdOptions.GitPath, "gitPath", "/usr/local/bin/git", "path to native git installation")
 	rootCmd.PersistentFlags().StringVar(&rootCmdOptions.CICDOrigin, "cicdOrigin", "", "provide a git origin url where a pipeline can push things via token")
+	rootCmd.PersistentFlags().BoolVar(&rootCmdOptions.DetectCICDOrigin, "detectCicdOrigin", false, "auto detect a git origin url where a pipeline can push things via token")
 	rootCmd.PersistentFlags().BoolVar(&rootCmdOptions.CI, "ci", false, "detects if command is running in a ci")
 	rootCmd.PersistentFlags().BoolVar(&rootCmdOptions.SkipChecks, "skipChecks", false, "skip checks like accidentally creating git-flow branches from wrong source branch")
 	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
