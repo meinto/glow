f7f69f87b4c4fdea31789f6601cc0955bc969bcb
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-01-22 22:10:50 +0100

    New logging formatter

--- logging/logging.go
+++ logging/logging.go
@@ -5,12 +5,16 @@ import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 func Log() *logrus.Entry {
 	l := logrus.New()
-	l.SetFormatter(&logrus.TextFormatter{
-		ForceColors: true,
+	l.SetFormatter(&prefixed.TextFormatter{
+		ForceColors:     true,
+		ForceFormatting: true,
+		FullTimestamp:   true,
+		TimestampFormat: "15:04:05",
 	})
 
 	pc, _, _, _ := runtime.Caller(1)
@@ -18,7 +22,10 @@ func Log() *logrus.Entry {
 	parts := strings.Split(nameFull, "/")
 	name := strings.Split(nameFull, "/")[len(parts)-1]
 
+	nameParts := strings.Split(name, ".")
+	prefix := strings.Join(nameParts[:len(nameParts)-1], ".")
+
 	return l.WithFields(logrus.Fields{
-		"caller": name,
+		"prefix": prefix,
 	})
 }
