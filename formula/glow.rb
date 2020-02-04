3999844b9eb1d5a8bdadb9fdcc5b6c52acae7986
Tobias Meinhardt <tobias.meinhardt@mailbox.org>
2020-02-05 00:09:14 +0100

    Brew formula update for glow version v4.1.0

--- formula/glow.rb
+++ formula/glow.rb
@@ -2,16 +2,16 @@
 class Glow < Formula
   desc ""
   homepage ""
-  version "4.0.2"
+  version "4.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/meinto/glow/releases/download/v4.0.2/glow_4.0.2_darwin_x86_64.tar.gz"
-    sha256 "aa4f5910e9f06f537d329c2b732de61eeeb35772d64e77b1ebf43dc10ac29612"
+    url "https://github.com/meinto/glow/releases/download/v4.1.0/glow_4.1.0_darwin_x86_64.tar.gz"
+    sha256 "3fa6fffb0a506563a400d8c9d16cd035f4bf577d1b9c9ee4bab63dc4371ad8d6"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/meinto/glow/releases/download/v4.0.2/glow_4.0.2_linux_x86_64.tar.gz"
-      sha256 "dba476eca8b411d27e364a6ac13a0f6f5d51f3a94789837dcd63624cf2c054d0"
+      url "https://github.com/meinto/glow/releases/download/v4.1.0/glow_4.1.0_linux_x86_64.tar.gz"
+      sha256 "a42356ce19feec39f3254fb76a49a0160abce63e312e854fd42db357408ef465"
     end
   end
   
