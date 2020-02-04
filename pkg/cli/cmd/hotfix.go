1713615168f502f7c83fa4bc29dde6cd3e394814
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-02-04 21:56:05 +0100

    Rewrite hotfix command

--- pkg/cli/cmd/hotfix.go
+++ pkg/cli/cmd/hotfix.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/meinto/glow"
+	"github.com/meinto/glow/pkg/cli/cmd/internal/command"
 	"github.com/meinto/glow/pkg/cli/cmd/internal/util"
 	"github.com/spf13/cobra"
 )
@@ -14,54 +15,63 @@ var hotfixCmdOptions struct {
 	VersionFileType   string
 }
 
-func init() {
-	RootCmd.Cmd().AddCommand(hotfixCmd)
-
-	hotfixCmd.Flags().BoolVar(&hotfixCmdOptions.Push, "push", false, "push created hotfix branch")
-	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.PostHotfixScript, "postHotfix", "", "script that executes after switching to hotfix branch")
-	hotfixCmd.Flags().StringArrayVar(&hotfixCmdOptions.PostHotfixCommand, "postHotfixCommand", []string{}, "commands which should be executed after switching to hotfix branch")
-
-	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.VersionFile, "versionFile", "VERSION", "name of git-semver version file")
-	hotfixCmd.Flags().StringVar(&hotfixCmdOptions.VersionFileType, "versionFileType", "raw", "git-semver version file type")
+type HotfixCommand struct {
+	command.Service
 }
 
-var hotfixCmd = &cobra.Command{
-	Use:   "hotfix",
-	Short: "create a hotfix branch",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		g, err := util.GetGitClient()
-		util.ExitOnError(err)
+func (cmd *HotfixCommand) PostSetup(parent command.Service) command.Service {
+	parent.Add(cmd)
+	cmd.Cmd().Flags().BoolVar(&hotfixCmdOptions.Push, "push", false, "push created hotfix branch")
+	cmd.Cmd().Flags().StringVar(&hotfixCmdOptions.PostHotfixScript, "postHotfix", "", "script that executes after switching to hotfix branch")
+	cmd.Cmd().Flags().StringArrayVar(&hotfixCmdOptions.PostHotfixCommand, "postHotfixCommand", []string{}, "commands which should be executed after switching to hotfix branch")
 
-		pathToRepo, _, _, err := g.GitRepoPath()
-		util.ExitOnError(err)
+	cmd.Cmd().Flags().StringVar(&hotfixCmdOptions.VersionFile, "versionFile", "VERSION", "name of git-semver version file")
+	cmd.Cmd().Flags().StringVar(&hotfixCmdOptions.VersionFileType, "versionFileType", "raw", "git-semver version file type")
+	return cmd
+}
 
-		version, s := util.ProcessVersion(
-			args[0],
-			hotfixCmdOptions.VersionFile,
-			hotfixCmdOptions.VersionFileType,
-			pathToRepo,
-		)
+var hotfixCmd = SetupHotfixCommand(RootCmd)
 
-		hotfix, err := glow.NewHotfix(version)
-		util.ExitOnError(err)
+func SetupHotfixCommand(parent command.Service) command.Service {
+	return command.Setup(&HotfixCommand{
+		&command.Command{
+			Command: &cobra.Command{
+				Use:   "hotfix",
+				Short: "create a hotfix branch",
+				Args:  cobra.MinimumNArgs(1),
+			},
+			Run: func(cmd command.Service, args []string) {
+				pathToRepo, _, _, err := cmd.GitClient().GitRepoPath()
+				util.ExitOnError(err)
 
-		util.ExitOnError(g.Create(hotfix, rootCmdOptions.SkipChecks))
-		util.ExitOnError(g.Checkout(hotfix))
+				version, s := util.ProcessVersion(
+					args[0],
+					hotfixCmdOptions.VersionFile,
+					hotfixCmdOptions.VersionFileType,
+					pathToRepo,
+				)
 
-		if util.IsSemanticVersion(args[0]) {
-			util.ExitOnError(s.SetNextVersion(args[0]))
-		} else {
-			util.ExitOnError(s.SetVersion(version))
-		}
-	},
-	PostRun: func(cmd *cobra.Command, args []string) {
-		util.PostRunWithCurrentVersion(
-			hotfixCmdOptions.VersionFile,
-			hotfixCmdOptions.VersionFileType,
-			hotfixCmdOptions.PostHotfixScript,
-			hotfixCmdOptions.PostHotfixCommand,
-			hotfixCmdOptions.Push,
-		)
-	},
+				hotfix, err := glow.NewHotfix(version)
+				util.ExitOnError(err)
+
+				util.ExitOnError(cmd.GitClient().Create(hotfix, rootCmdOptions.SkipChecks))
+				util.ExitOnError(cmd.GitClient().Checkout(hotfix))
+
+				if util.IsSemanticVersion(args[0]) {
+					util.ExitOnError(s.SetNextVersion(args[0]))
+				} else {
+					util.ExitOnError(s.SetVersion(version))
+				}
+			},
+			PostRun: func(cmd command.Service, args []string) {
+				util.PostRunWithCurrentVersion(
+					hotfixCmdOptions.VersionFile,
+					hotfixCmdOptions.VersionFileType,
+					hotfixCmdOptions.PostHotfixScript,
+					hotfixCmdOptions.PostHotfixCommand,
+					hotfixCmdOptions.Push,
+				)
+			},
+		},
+	})
 }
