33c38a313c82c303f85d5987cf9eca6ad5f0ff6a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-02-04 22:07:25 +0100

    Rewrite close hotfix command

--- pkg/cli/cmd/close.hotfix.go
+++ pkg/cli/cmd/close.hotfix.go
@@ -4,52 +4,60 @@ import (
 	l "github.com/meinto/glow/logging"
 
 	"github.com/meinto/glow"
+	"github.com/meinto/glow/pkg/cli/cmd/internal/command"
 	"github.com/meinto/glow/pkg/cli/cmd/internal/util"
 	"github.com/meinto/glow/semver"
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	closeCmd.Cmd().AddCommand(closeHotfixCmd)
-	util.AddFlagsForMergeRequests(closeHotfixCmd)
+type CloseHotfixCommand struct {
+	command.Service
 }
 
-var closeHotfixCmd = &cobra.Command{
-	Use:   "hotfix",
-	Short: "close a release branch",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		version := args[0]
+func (cmd *CloseHotfixCommand) PostSetup(parent command.Service) command.Service {
+	parent.Add(cmd)
+	util.AddFlagsForMergeRequests(cmd.Cmd())
+	return cmd
+}
 
-		if version == "current" {
-			g, err := util.GetGitClient()
-			util.ExitOnError(err)
+var closeHotfixCmd = SetupCloseHotfixCommand(RootCmd)
 
-			pathToRepo, _, _, err := g.GitRepoPath()
-			util.ExitOnError(err)
+func SetupCloseHotfixCommand(parent command.Service) command.Service {
+	return command.Setup(&CloseHotfixCommand{
+		&command.Command{
+			Command: &cobra.Command{
+				Use:   "hotfix",
+				Short: "close a release branch",
+				Args:  cobra.MinimumNArgs(1),
+			},
+			Run: func(cmd command.Service, args []string) {
+				version := args[0]
 
-			s := semver.NewSemverService(
-				pathToRepo,
-				"/bin/bash",
-				releaseCmdOptions.VersionFile,
-				releaseCmdOptions.VersionFileType,
-			)
-			v, err := s.GetCurrentVersion()
-			util.ExitOnError(err)
-			version = v
-		}
+				if version == "current" {
+					pathToRepo, _, _, err := cmd.GitClient().GitRepoPath()
+					util.ExitOnError(err)
 
-		gp, err := util.GetGitProvider()
-		util.ExitOnError(err)
+					s := semver.NewSemverService(
+						pathToRepo,
+						"/bin/bash",
+						releaseCmdOptions.VersionFile,
+						releaseCmdOptions.VersionFileType,
+					)
+					v, err := s.GetCurrentVersion()
+					util.ExitOnError(err)
+					version = v
+				}
 
-		currentBranch, err := glow.NewHotfix(version)
-		util.ExitOnError(err)
+				currentBranch, err := glow.NewHotfix(version)
+				util.ExitOnError(err)
 
-		err = gp.Close(currentBranch)
-		if !util.MergeRequestFlags.Gracefully {
-			util.ExitOnError(err)
-		} else {
-			l.Log().Error(err)
-		}
-	},
+				err = cmd.GitProvider().Close(currentBranch)
+				if !util.MergeRequestFlags.Gracefully {
+					util.ExitOnError(err)
+				} else {
+					l.Log().Error(err)
+				}
+			},
+		},
+	})
 }
